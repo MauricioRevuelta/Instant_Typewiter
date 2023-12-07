de5f67083b0f167d5ba94e3708d2cbab1045cfa8
Mauricio Revuelta <mauriciorevueltas@gmail.com>
2023-12-06 23:44:12 -0500

    Adding more pages
    Two more pages were added to the website and some animation was also added to the Title.

--- js/index.js
+++ js/index.js
@@ -1,89 +1,62 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const plus = document.querySelector(".plus");
-    const minus = document.querySelector(".minus");
-    const number = document.querySelector(".number");
-
-    let a = 1;
-
-    plus.addEventListener("click", () => {
-        if(a<10) {
-            a++;
-            number.innerText = a;
-            console.log(a);
+document.addEventListener('DOMContentLoaded', function () {
+    function initializeCounter(plusSelector, minusSelector, numberSelector) {
+      const plus = document.querySelector(plusSelector);
+      const minus = document.querySelector(minusSelector);
+      const number = document.querySelector(numberSelector);
+  
+      let count = 1;
+  
+      plus.addEventListener('click', () => {
+        if (count < 10) {
+          count++;
+          number.innerText = count;
+          console.log(count);
         }
-    });
-
-    minus.addEventListener("click", () => {
-        if(a>1) {
-            a--;
-            number.innerText = a;
-            console.log(a);
+      });
+  
+      minus.addEventListener('click', () => {
+        if (count > 1) {
+          count--;
+          number.innerText = count;
+          console.log(count);
         }
+      });
+    }
+  
+    initializeCounter('.plus', '.minus', '.number');
+    initializeCounter('.plus2', '.minus2', '.number2');
+    initializeCounter('.plus3', '.minus3', '.number3');
+    initializeCounter('.plus4', '.minus4', '.number4');
+  });
+  
+  let typing = new Typed('.typing', {
+    strings: ['Instant Typewriter'],
+    loop: !0,
+    typeSpeed: 120,
+    backSpeed: 80,
+  });
+  
+  AOS.init(),
+    scrollButton &&
+      (window.addEventListener('scroll', () => {
+        pageYOffset > 1.2 * window.innerHeight
+          ? (scrollButton.style.display = 'flex')
+          : (scrollButton.style.display = 'none');
+      }),
+      scrollButton.addEventListener('click', () => {
+        window.scrollTo(0, 0);
+      })),
+    hamburgerMenu.addEventListener('click', () => {
+      navContent.classList.add('show'), (document.body.style.overflow = 'hidden');
+    }),
+    closeNavButton.addEventListener('click', () => {
+      navContent.classList.remove('show'),
+        (document.body.style.overflow = 'initial');
+    }),
+    navLinks.forEach((e) => {
+      e.addEventListener('click', () => {
+        navContent.classList.remove('show'),
+          (document.body.style.overflow = 'initial');
+      });
     });
-
-    const plus2 = document.querySelector(".plus2");
-    const minus2 = document.querySelector(".minus2");
-    const number2 = document.querySelector(".number2");
-
-    let b = 1;
-
-    plus2.addEventListener("click", () => {
-        if(b<10) {
-            b++;
-            number2.innerText = b;
-            console.log(b);
-        }
-    });
-
-    minus2.addEventListener("click", () => {
-        if(b>1) {
-            b--;
-            number2.innerText = b;
-            console.log(b);
-        }
-    });
-
-    const plus3 = document.querySelector(".plus3");
-    const minus3 = document.querySelector(".minus3");
-    const number3 = document.querySelector(".number3");
-
-    let c = 1;
-
-    plus3.addEventListener("click", () => {
-        if(c<10) {
-            c++;
-            number3.innerText = c;
-            console.log(c);
-        }
-    });
-
-    minus3.addEventListener("click", () => {
-        if(c>1) {
-            c--;
-            number3.innerText = c;
-            console.log(c);
-        }
-    });
-
-    const plus4 = document.querySelector(".plus4");
-    const minus4 = document.querySelector(".minus4");
-    const number4 = document.querySelector(".number4");
-
-    let d = 1;
-
-    plus4.addEventListener("click", () => {
-        if(d<10) {
-            d++;
-            number4.innerText = d;
-            console.log(d);
-        }
-    });
-
-    minus4.addEventListener("click", () => {
-        if(d>1) {
-            d--;
-            number4.innerText = d;
-            console.log(d);
-        }
-    });
-});
+  
