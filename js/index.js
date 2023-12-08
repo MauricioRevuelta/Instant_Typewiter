b2cc81df0475e82fb84cc8b090228f8a79951a57
Alfonso-Ramirez <joaqalf@gmail.com>
2023-12-07 22:31:03 -0400

    changes on javascript and contact form

--- js/index.js
+++ js/index.js
@@ -1,62 +1,59 @@
 document.addEventListener('DOMContentLoaded', function () {
-    function initializeCounter(plusSelector, minusSelector, numberSelector) {
-      const plus = document.querySelector(plusSelector);
-      const minus = document.querySelector(minusSelector);
-      const number = document.querySelector(numberSelector);
-  
-      let count = 1;
-  
-      plus.addEventListener('click', () => {
-        if (count < 10) {
-          count++;
-          number.innerText = count;
-          console.log(count);
-        }
-      });
-  
-      minus.addEventListener('click', () => {
-        if (count > 1) {
-          count--;
-          number.innerText = count;
-          console.log(count);
-        }
-      });
-    }
-  
-    initializeCounter('.plus', '.minus', '.number');
-    initializeCounter('.plus2', '.minus2', '.number2');
-    initializeCounter('.plus3', '.minus3', '.number3');
-    initializeCounter('.plus4', '.minus4', '.number4');
-  });
-  
-  let typing = new Typed('.typing', {
-    strings: ['Instant Typewriter'],
-    loop: !0,
-    typeSpeed: 120,
-    backSpeed: 80,
-  });
-  
-  AOS.init(),
-    scrollButton &&
-      (window.addEventListener('scroll', () => {
-        pageYOffset > 1.2 * window.innerHeight
-          ? (scrollButton.style.display = 'flex')
-          : (scrollButton.style.display = 'none');
-      }),
-      scrollButton.addEventListener('click', () => {
-        window.scrollTo(0, 0);
-      })),
-    hamburgerMenu.addEventListener('click', () => {
-      navContent.classList.add('show'), (document.body.style.overflow = 'hidden');
-    }),
-    closeNavButton.addEventListener('click', () => {
-      navContent.classList.remove('show'),
-        (document.body.style.overflow = 'initial');
-    }),
-    navLinks.forEach((e) => {
-      e.addEventListener('click', () => {
-        navContent.classList.remove('show'),
-          (document.body.style.overflow = 'initial');
-      });
+  function initializeCounter(plusSelector, minusSelector, numberSelector) {
+    const plus = document.querySelector(plusSelector);
+    const minus = document.querySelector(minusSelector);
+    const number = document.querySelector(numberSelector);
+
+    let count = 1;
+
+    plus.addEventListener('click', () => {
+      if (count < 10) {
+        count++;
+        number.innerText = count;
+        console.log(count);
+      }
     });
-  
+
+    minus.addEventListener('click', () => {
+      if (count > 1) {
+        count--;
+        number.innerText = count;
+        console.log(count);
+      }
+    });
+  }
+
+  initializeCounter(
+    '.plus',
+    '.minus',
+    '.number',
+    '.plus2',
+    '.minus2',
+    '.number2',
+    '.plus3',
+    '.minus3',
+    '.number3',
+    '.plus4',
+    '.minus4',
+    '.number4'
+  );
+});
+
+let typing = new Typed('.typing', {
+  strings: ['Instant Typewriter'],
+  loop: !0,
+  typeSpeed: 120,
+  backSpeed: 80,
+});
+
+function showMessage() {
+  // Display SweetAlert2 modal
+  Swal.fire({
+    title: 'Congratulation!',
+    text: 'Your message has been sent!',
+    icon: 'success',
+    confirmButtonText: 'OK',
+  });
+}
+
+document.getElementById('submit').addEventListener('click', showMessage);
